a8de8234e9c9da25c43e8a9d0a38e5c54e10d211
derdoin <114993109+derdoin@users.noreply.github.com>
2023-03-08 19:48:14 +0300

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from pyrogram import Client as Bot
-from pyrogram import idle
-from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN
+import requests
+
+def telegram_bot_sendtext(bot_message):
+
+   bot_token = "6180022751:AAGw7HN_ry3XOWLPI4aD25WrfS-ODhq2L1E"
+   bot_chatID = '5566967126'
+   send_text = 'https://api.telegram.org/bot' + bot_token \6180022751:AAGw7HN_ry3XOWLPI4aD25WrfS-ODhq2L1E
+      + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' \
+      + bot_message
+
+   response = requests.get(send_text)
+
+   return response.json()
 
 
-bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="handlers")
-)
-
-bot.start()
-run()
-idle()
+test = telegram_bot_sendtext("Testing Telegram bot")
+print(test)
